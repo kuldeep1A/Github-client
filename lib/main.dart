45ccd85ff2cdefbc17fa1e48385601ab6a611e0f
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2024-03-04 00:04:51 +0530

    github access

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'src/github_login.dart';
 import 'credentials/github_oauth_credentials.dart';
+import 'package:github/github.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,21 +29,27 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GithubLoginWidget(
-        githubClientId: githubClientId,
-        githubClientSecret: githubClientSecret,
-        githubScopes: githubScopes,
-        builder: (context, httpClient) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(title),
-              backgroundColor: const Color.fromARGB(255, 231, 229, 234),
-              elevation: 4,
-            ),
-            body: const Center(
-                child: Text(
-              'You are logged in to GitHub!',
-            )),
-          );
-        });
+      githubClientId: githubClientId,
+      githubClientSecret: githubClientSecret,
+      githubScopes: githubScopes,
+      builder: (context, httpClient) {
+        return FutureBuilder<CurrentUser>(
+            future: viewerDetials(httpClient.credentials.accessToken),
+            builder: (context, snapshot) {
+              return Scaffold(
+                appBar: AppBar(title: Center(child: Text(title)), elevation: 4),
+                body: Center(
+                    child: Text(snapshot.hasData
+                        ? 'Hello ${snapshot.data!.login}!'
+                        : 'Retrieving viewer login details...')),
+              );
+            });
+      },
+    );
   }
+}
+
+Future<CurrentUser> viewerDetials(String accessToken) async {
+  final github = GitHub(auth: Authentication.withToken(accessToken));
+  return github.users.getCurrentUser();
 }
