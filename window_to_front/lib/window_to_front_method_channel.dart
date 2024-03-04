a2d7b868fb30fe727acbecaec83fddd6c9953df9
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2024-03-05 00:48:50 +0530

    Revert "remove testing - @visibleForTesting"
    This reverts commit 7c7e1dcb78bb42a642f690137b5677748de94d33.

--- window_to_front/lib/window_to_front_method_channel.dart
+++ window_to_front/lib/window_to_front_method_channel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 import 'window_to_front_platform_interface.dart';
@@ -5,6 +6,7 @@ import 'window_to_front_platform_interface.dart';
 /// An implementation of [WindowToFrontPlatform] that uses method channels.
 class MethodChannelWindowToFront extends WindowToFrontPlatform {
   /// The method channel used to interact with the native platform.
+  @visibleForTesting
   final methodChannel = const MethodChannel('window_to_front');
 
   // Replace the getPlatformVersion() with the following implementation
