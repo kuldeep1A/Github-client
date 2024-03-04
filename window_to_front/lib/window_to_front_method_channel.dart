7784addcbe181a95a90f0101f1b93ba5bd7a305c
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2024-03-05 01:32:09 +0530

    SHA a2d7b868fb3*********d6c9953df9

--- window_to_front/lib/window_to_front_method_channel.dart
+++ window_to_front/lib/window_to_front_method_channel.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 import 'window_to_front_platform_interface.dart';
@@ -6,7 +5,6 @@ import 'window_to_front_platform_interface.dart';
 /// An implementation of [WindowToFrontPlatform] that uses method channels.
 class MethodChannelWindowToFront extends WindowToFrontPlatform {
   /// The method channel used to interact with the native platform.
-  @visibleForTesting
   final methodChannel = const MethodChannel('window_to_front');
 
   // Replace the getPlatformVersion() with the following implementation
